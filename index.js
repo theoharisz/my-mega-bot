6b0983a51e0ce7868b122e13a0adbd2e3a22724d
theoharisz <44214062+theoharisz@users.noreply.github.com>
2018-10-17 01:13:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  if (cmd === `${prefix}ping`){
-    message.channel.send("Pong!");
+  if (cmd === `${prefix}hi`){
+    message.channel.send("gamo to soi sou!");
   }
 });
 
